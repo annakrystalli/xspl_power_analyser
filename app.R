82565206fa48a342e39948c7db00185f1a75beb7
annakrystalli <annakrystalli@googlemail.com>
2017-12-12 17:37:12 +0000

    coomit working draft app

--- app.R
+++ app.R
@@ -11,6 +11,8 @@ library(shiny)
 library(pwr)
 library(tidyverse)
 library(plotly)
+library(shinydashboard)
+
 # effect = d
 # group_n = n
 
@@ -21,70 +23,97 @@ d <- seq(.1,1,.1)
 n <- seq(10,110,10)
 
 df <- expand.grid(d = d, n = n) %>% as_tibble %>% 
-    mutate(pwr = pwr.t.test(n, d, sig.level=0.05, power=NULL,
+    mutate(pwr = pwr.t.test(n, d, sig.level = 0.05, power = NULL,
                             type="two.sample",
                             alternative="two.sided")$power,
            d = as.factor(d))
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Power estimation for 2 sample t-test"),
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("d",
-                     "effect size:",
-                     min = 0,
-                     max = 1,
-                     step = 0.1,
-                     value = 0.5),
-         sliderInput("pwr",
-                     "power:",
-                     min = 0,
-                     max = 1,
-                     step = 0.1,
-                     value = 0.8),
-         br(),
-         h4("Sample size needed IN EACH group"),
-         h1(textOutput("n"))
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-          plotlyOutput("plot")
-      )
-   )
-)
+ui <- dashboardPage(skin = "black",
+    dashboardHeader(title = "Power estimation for 2 sample t-test",
+                    titleWidth = 450),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Dashboard", tabName = "dashboard"),
+            menuItem("Raw data", tabName = "rawdata")
+        ),
+        sliderInput("d",
+                    "effect size:",
+                    min = 0,
+                    max = 1,
+                    step = 0.1,
+                    value = 0.5)),
+    dashboardBody(
+        tabItems(
+            tabItem("dashboard",
+                    fluidRow(valueBoxOutput("n", width = 5),
+                             valueBoxOutput("savings", width = 5)),
+                    sliderInput("pwr",
+                                "power:",
+                                min = 0,
+                                max = 1,
+                                step = 0.1,
+                                value = 0.8),
+                    fluidRow(box(title = "Power trend ~ effect size", background = "teal", solidHeader = TRUE,
+                        plotOutput("plot"), width = 10))
+                    ),
+            
+            tabItem("rawdata")
+            )
+        )
+    )
+    
+    
+    
+ 
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$plot <- renderPlotly({
-      # generate bins based on input$bins from ui.R
-       
-      p <- df %>% filter(d == input$d) %>% 
-           ggplot(aes(n, pwr, group = d, color = d)) + geom_line() +
-           labs(title ="",
-                subtitle = "",
-                color = "effect size") +
-           xlab("Sample Size IN EACH GROUP (n)") +
-           ylab("Power") +
-          theme_minimal() + ylim(0, max(d))
+    
 
-      
-      #print(ggplotly(p))
-      
-      p
-      
-
-   })
-   output$n <- renderText({
-       pwr.t.test(n = NULL, d = input$d, sig.level=0.05, power=input$pwr,
-                  type="two.sample",
-                  alternative="two.sided")$n %>% as.integer()
-   })
+    
+    output$plot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        
+        p <- df %>% filter(d == input$d) %>% 
+            ggplot(aes(n, pwr, group = d, color = d)) + geom_line() +
+            labs(title ="",
+                 subtitle = "",
+                 color = "effect size") +
+            xlab("Sample Size IN EACH GROUP (n)") +
+            ylab("Power")  + ylim(0, max(d)) +
+            theme_hc(bgcolor = "darkunica") +
+            scale_colour_hc("darkunica") + 
+            theme(axis.text = element_text(colour = "white"),
+                  panel.grid.major = element_line(colour = "grey50")) +
+            geom_vline(xintercept = pwr.t.test(n = NULL, d = input$d, sig.level = 0.05, 
+                                               power=input$pwr, type="two.sample",
+                                               alternative="two.sided")$n %>% as.integer(),
+                       colour = "white")
+        
+        
+        #print(ggplotly(p, tooltip = c("n", "d", "pwr")))
+        
+        print(p)
+        
+        
+    })
+    output$n <- renderValueBox({
+        valueBox(subtitle = "min n required for EACH group", color  = "teal", 
+                 value = pwr.t.test(n = NULL, d = input$d, sig.level = 0.05, 
+                            power=input$pwr, type="two.sample",
+                            alternative="two.sided")$n %>% as.integer(),
+            icon = icon("users")
+        )
+    })
+    output$savings <- renderValueBox({
+        valueBox(subtitle = "Savings", color  = "lime", 
+                 value = paste("£", pwr.t.test(n = NULL, d = input$d, sig.level = 0.05, 
+                                    power=input$pwr, type="two.sample",
+                                    alternative="two.sided")$n %>% as.integer()*0.2 *100),
+                 icon = icon("money")
+        )
+    })
 }
 
 # Run the application 
